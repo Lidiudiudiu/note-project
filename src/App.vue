2f7671b611ecc4cf2aa596cbce3008e1be744182
Lidiudiudiu <08223241@cumt.edu.cn>
2024-10-21 23:16:47 +0800

    样式书写

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,18 @@
 </script>
 
 <template>
-
-  <router-view />
+  <div class="box">
+    <router-view />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+/* app.vue */
+.box {
+  width: 100vw;
+  height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  flex-flow: column nowrap;
+}
+</style>
